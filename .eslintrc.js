3fa3b5e7e2788ecc880da054975381952019c5f5
Jason Guo <jasonguo2013jg@gmail.com>
2021-02-13 13:04:57 -0400

    Initial configs

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: { 'prettier/prettier': ['error', { endOfLine: 'auto' }] },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
